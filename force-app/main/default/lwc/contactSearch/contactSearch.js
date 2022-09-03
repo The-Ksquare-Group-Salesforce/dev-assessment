0f5137f0f4bdbb589e49f7748a422511d0d6669a
JoseAriasTKG <jose.arias@theksquaregroup.com>
2022-09-03 18:29:15 -0400

    Fixed minor bug where all contacts showed

--- force-app/main/default/lwc/contactSearch/contactSearch.js
+++ force-app/main/default/lwc/contactSearch/contactSearch.js
@@ -21,44 +21,44 @@ export default class ContactSearch extends LightningElement {
   }
   searchContact(event) {
     if (event.target.value.length >= 3) {
-      let searchedTempContacts = [];
       this.searchKey = event.target.value.toLowerCase();
-      for (const contact of this.mainContacts) {
+      /*for (const contact of this.mainContacts) {
         if (contact.Name.toLowerCase().includes(this.searchKey)) {
           searchedTempContacts.push(contact);
         }
         this.contacts = searchedTempContacts;
-      }
+      }*/
+      this.loadData();
     }
-    if (event.detail.value.length == 0)
+    else if (event.target.value.length == 0)
       this.contacts = this.mainContacts;
   }
   loadData() {
-    if (this.contacts.length == 0) {
-      return getContacts({ limitSize: this.limitSize, offSet: this.offSet, accountId: this.recordId }).then(response => {
+    if (this.mainContacts.length > 0) {
+      return getContactsOnSearch({ contactName: this.searchKey, accountId: this.recordId }).then(response => {
+        let searchedTempContacts = [];
         this.contacts = response;
-        this.mainContacts = response;
         this.error = undefined;
+        for (const contact of this.mainContacts) {
+          if (contact.Name.toLowerCase().includes(this.searchKey)) {
+            searchedTempContacts.push(contact);
+          }
+          this.contacts = searchedTempContacts;
+        }
 
       }).catch(error => {
         this.error = error;
         this.contacts = undefined;
       })
     }
-    /*return getContactsOnSearch({ contactName: this.searchKey, accountId: this.recordId }).then(response=>{
+    return getContacts({ limitSize: this.limitSize, offSet: this.offSet, accountId: this.recordId }).then(response => {
       this.contacts = response;
-      console.log(this.contacts);
+      this.mainContacts = response;
       this.error = undefined;
-      for(const contact of this.mainContacts){
-        if(contact.Name.toLowerCase().includes(this.searchKey)){
-          this.searchedTempContacts.push(contact);
-        }
-        this.contacts = this.searchedTempContacts;
-      }
 
-    }).catch(error=>{
+    }).catch(error => {
       this.error = error;
       this.contacts = undefined;
-    })*/
+    })
   }
 }
