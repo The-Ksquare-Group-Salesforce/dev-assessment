23d926f01df21193285e7643e9b218ef5cc17845
Frank <jose.perez@htheksquaregroup.com>
2022-09-07 11:03:23 -0500

    updates to classes

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,52 +1,42 @@
 import { LightningElement, api, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
-import getContactsByAccountId from '@salesforce/apex/ContactController.getContactsByAccountId';
 import getContactsByAccount from '@salesforce/apex/ContactController.getContactsByAccount';
 import { NavigationMixin } from 'lightning/navigation';
-// const columns = [
-//     {label: 'First Name', fieldName: 'FirstName'},
-//     {label: 'Last Name', fieldName: 'LastName'},
-//     {label: 'Email', fieldName: 'email'},
-// ]
+
 const actions = [
     { label: 'View', name: 'show_details' },
     { label: 'Edit', name: 'edit_details' },
 ];
 
-    export default class ContactRecords extends NavigationMixin(LightningElement) {
+export default class ContList extends NavigationMixin(LightningElement) {
   
-        columns =   [
-            {type : 'action', typeAttributes: {rowActions: actions,}},
-            {label: 'First Name', fieldName: 'FirstName'},
-        {label: 'Last Name', fieldName: 'LastName'},
-        {label: 'Email', fieldName: 'Email'},
-        {label: 'Phone', fieldName: 'Phone'},
-        {label: 'Title', fieldName: 'Title'},
-        ];
-    searchTerm = '';
-    searchTermForButton = '';
+    columns =   [
+        {type : 'action',     typeAttributes: {rowActions: actions,}},
+        {label: 'First Name', fieldName: 'FirstName'},
+        {label: 'Last Name',  fieldName: 'LastName'},
+        {label: 'Email',      fieldName: 'Email'},
+        {label: 'Phone',      fieldName: 'Phone'},
+        {label: 'Title',      fieldName: 'Title'},
+    ];
+
+    searchStr = '';
+    searchStrBtn = '';
     @api recordId;
     contactId;
-    //@wire(getContactsByAccountId, {accountId: '$recordId'})contacts;
-    //@wire(getRecord, {recordId: '$recordId'})Account;
 
-    @wire(getContactsByAccount, {searchTerm: '$searchTermForButton',accountId: '$recordId'}) contacts;
+    @wire(getContactsByAccount, {searchStr: '$searchStrBtn',accountId: '$recordId'}) contacts;
 
-    handleSearchTermChange(event) {
-		// Debouncing this method: do not update the reactive property as
-		// long as this function is being called within a delay of 300 ms.
-		// This is to avoid a very large number of Apex method calls.
+    handleSearchStrChange(event) {
 		window.clearTimeout(this.delayTimeout);
-		const searchTerm = event.target.value;
-		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		const searchStr = event.target.value;
+
 		this.delayTimeout = setTimeout(() => {
-			this.searchTerm = searchTerm;
+			this.searchStr = searchStr;
 		}, 300);
 	}
 
     handleOnClickButton(event){
 
-        this.searchTermForButton = this.searchTerm;
+        this.searchStrBtn = this.searchStr;
     }
 
     navigateToRecordViewPage(event) {
@@ -57,7 +47,7 @@ const actions = [
                 type: 'standard__recordPage',
                 attributes: {
                     recordId: event.detail.row.Id,
-                    objectApiName: 'Contact', // objectApiName is optional
+                    objectApiName: 'Contact',
                     actionName: 'view',
                 }
             });
@@ -69,13 +59,10 @@ const actions = [
                 type: 'standard__recordPage',
                 attributes: {
                     recordId: event.detail.row.Id,
-                    objectApiName: 'Contact', // objectApiName is optional
+                    objectApiName: 'Contact',
                     actionName: 'edit',
                 }
             });
-
         }
-
-
     }
 }
